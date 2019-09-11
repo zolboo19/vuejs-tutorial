26ee7c202e045a6ab1975904ed6fdeccb372363b
zolboo19 <zolboo0906@gmail.com>
2019-09-11 17:44:00 +0800

    component communications 1

--- vue-test3/src/components/User.vue
+++ vue-test3/src/components/User.vue
@@ -2,10 +2,15 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change name</button>
+        <p>{{ name }}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail 
+                    :myName="name" 
+                    @nameWasReset="name = $event" 
+                    :resetFn="resetName"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -19,6 +24,19 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data: function(){
+            return{
+                name: 'Zolboo'
+            }
+        },
+        methods:{
+            changeName(){
+                this.name = 'Saraa';
+            },
+            resetName(){
+                this.name = 'Zolboo'
+            }
+        },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
--- vue-test3/src/components/UserDetail.vue
+++ vue-test3/src/components/UserDetail.vue
@@ -2,10 +2,31 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>Хэрэглэгчийн нэр: {{ switchName() }}</p>
+        <button @click="resetFn()">Нэр дахин тохируулах</button>
     </div>
 </template>
 
 <script>
+    export default{
+        props: {
+            myName: {
+                type: String,
+                //required: true
+                default: 'Tsogoo'
+            },
+            resetFn: Function
+        },
+        methods:{
+            switchName(){
+                return this.myName.split("").reverse("").join("");
+            },
+            resetName(){
+                this.myName = 'Zolboo';
+                this.$emit('nameWasReset', this.myName)
+            }
+        }
+    }
 </script>
 
 <style scoped>
