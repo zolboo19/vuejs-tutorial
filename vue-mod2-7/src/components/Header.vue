538b9bc2b7a7a536d4eae347a237e0c52dfb5ea2
zolboo19 <zolboo0906@gmail.com>
2019-12-21 12:06:42 +0800

    Vuejs router hash and History mode Navigation menu, active links

---
+++ vue-mod2-7/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="nav nav-pills">
+    <router-link to="/" tag="li" active-class="active" exact>
+      <a>Нүүр хуудас</a>
+    </router-link>
+    <router-link to="/user" tag="li" active-class="active">
+      <a>Хэрэглэгч</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- vue-mod2-7/src/components/Users/User.vue
+++ vue-mod2-7/src/components/Users/User.vue
@@ -1,9 +1,18 @@
 <template>
-  <h1>User</h1>
+  <div>
+    <h1>User</h1>
+    <button @click="goToHome">Нүүр хуудасруу шилжих</button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    goToHome() {
+      this.$router.push({ path: "/" });
+    }
+  }
+};
 </script>
 
 <style>
