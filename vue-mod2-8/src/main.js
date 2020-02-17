3378ecfc683e4f52450d63cafc22e68927a11e5e
zolboo19 <zolboo0906@gmail.com>
2020-02-17 18:12:49 +0800

    Vue-resource install and setup

--- vue-mod2-8/src/main.js
+++ vue-mod2-8/src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueResource from "vue-resource";
+import App from "./App.vue";
+
+Vue.use(VueResource);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
