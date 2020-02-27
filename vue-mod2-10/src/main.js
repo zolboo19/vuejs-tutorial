df581ada709a86c8ee63aa9380f7efd0acb095a8
zolboo19 <zolboo0906@gmail.com>
2020-02-28 07:28:39 +0800

    using interceptors

--- vue-mod2-10/src/main.js
+++ vue-mod2-10/src/main.js
@@ -9,6 +9,20 @@ axios.defaults.baseURL = "https://vue-axios-5ca69.firebaseio.com";
 axios.defaults.headers.common["Authorization"] = "adsfadsfasg";
 axios.defaults.headers.get["Content-Type"] = "application/json";
 
+const reqInterceptors = axios.interceptors.request.use(config => {
+  console.log("Request interceptors ", config);
+  //config.headers.common.Authorization = "Helo";
+  return config;
+});
+
+const resInterceptors = axios.interceptors.response.use(res => {
+  console.log("Response interceptors ", res);
+  return res;
+});
+
+axios.interceptors.request.eject(reqInterceptors);
+axios.interceptors.response.eject(resInterceptors);
+
 new Vue({
   el: "#app",
   router,
