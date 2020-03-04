a33b03a7a108fccb56d76ba1291cb35b810b941a
zolboo19 <zolboo0906@gmail.com>
2020-03-05 07:33:32 +0800

    install material design and Navbar

--- vue-mod3-cli/ninja-default/src/main.js
+++ vue-mod3-cli/ninja-default/src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
